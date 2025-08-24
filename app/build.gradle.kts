c29f4e4ee52a6c15805997532394bde215ae862f
Bhagyesh Patil <bhagyeshp013@gmail.com>
2025-08-24 13:29:17 +0530

    feat: configure Firebase and add dependencies
    - Set up Firebase with Authentication, Firestore, and Cloud Messaging
    - Added Firebase dependencies to project
    - Included google-services.json in .gitignore to prevent it from being tracked by git

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -53,6 +54,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Firebase
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.firestore)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
